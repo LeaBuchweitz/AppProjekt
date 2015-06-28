15e1ea81365deb61bdfe357b4c8167338a5b372e
Lea Buchweitz <lea.buchweitz@gmx.de>
2015-06-28 15:04:15 +0200

    Window with introductions how to mark a gap

--- KnowledgeToGo/app/src/main/java/com/learningapp/infoproject/knowledgetogo/Add_Gap_Question_Activity.java
+++ KnowledgeToGo/app/src/main/java/com/learningapp/infoproject/knowledgetogo/Add_Gap_Question_Activity.java
@@ -7,10 +7,14 @@ import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -20,6 +24,7 @@ public class Add_Gap_Question_Activity extends ActionBarActivity {
     private int lid;
     private int uid;
     private boolean uploaded;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +34,33 @@ public class Add_Gap_Question_Activity extends ActionBarActivity {
         uploaded = false;
 
         // Get Info from SharedPreferences for User-ID
-        SharedPreferences prefs = getSharedPreferences("com.learningapp.infoproject.knowledgetogo", Context.MODE_PRIVATE);
+        prefs = getSharedPreferences("com.learningapp.infoproject.knowledgetogo", Context.MODE_PRIVATE);
         uid = prefs.getInt("User-ID",0);
+
+        // DialogBox with instructions how to mark a gap
+        if(prefs.getBoolean("Instruction",true)) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            LayoutInflater inflater = this.getLayoutInflater();
+            View view = inflater.inflate(R.layout.dialog_add_gap_question, null);
+            dialog.setCancelable(true);
+            TextView text = (TextView) view.findViewById(R.id.instruction);
+            text.setGravity(Gravity.CENTER);
+            dialog.setView(view);
+            final CheckBox dontShow = (CheckBox) view.findViewById(R.id.dont_show);
+            dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Don't show dalogBox again, if CheckBox marked
+                    if(dontShow.isChecked()) {
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putBoolean("Instruction", false);
+                        editor.apply();
+                    }
+                    dialog.dismiss();
+                }
+            });
+            dialog.create().show();
+        }
     }
 
     public void sendQuestion(View view){
